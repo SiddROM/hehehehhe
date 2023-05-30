0372f01cb042a089b48864c53dcff462f5bbe1c6
SiddROM <121415948+SiddROM@users.noreply.github.com>
2023-05-30 10:05:30 +0530

    Update config.py

--- GodOfDeathRobot/config.py
+++ GodOfDeathRobot/config.py
@@ -1,15 +1,15 @@
 class Config(object):
     LOGGER = True
-    API_ID = 12345678
-    API_HASH = "apihashbytelegram"
-    BOT_USERNAME = "bot_username"
-    BOT_ID = 1234567890
-    BOT_TOKEN = "1234567890:ABCDe-1fg-hIJ23kl45MnOPQ6Rstu7vw8xY"
-    OWNER_ID = 9876543210
-    OWNER_USERNAME = ''
-    START_IMG = ''
-    UPDATES = ''
-    SUPPORT = ''
+    API_ID = 26019444
+    API_HASH = "a308fac723905cdbd836414b18f3b3d6"
+    BOT_USERNAME = "GodOfDeatAbot"
+    BOT_ID = 6189158301
+    BOT_TOKEN = "6189158301:AAF6vqyaaitiYgAhEAsL_hIUFTUFd1LmOr8"
+    OWNER_ID = 6046532356
+    OWNER_USERNAME = 'MonsterKatakuri'
+    START_IMG = 'https://te.legra.ph/file/858ac4f00901ded52191d.jpg'
+    UPDATES = 'RealmBotUpdates'
+    SUPPORT = 'RealmBotSupport'
     INFINITY_GODS = [1413518510] #Developers who can access the bots server and can execute, modify bot code. Can also manage other Forms.
     ULTIMATE_GODS = [] #Also called as Sudos, Gods can access, can gban, manage disasters lower than them and and admins.
     SUPERIORS = [] #Also known as Support users, These Gods have access to globally ban users across God.
